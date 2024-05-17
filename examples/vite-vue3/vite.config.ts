40311d32415d0ccb94ca2c9f4444ea8164762d60
bin.jin <bin.jin@d1m.cn>
2024-05-17 17:54:34 +0800

    feat: init

--- examples/vite-vue3/vite.config.ts
+++ examples/vite-vue3/vite.config.ts
@@ -5,24 +5,24 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    plugins: [vue(), template()],
-    server: {
-        port: 8080,
-        hmr: {
-            host: 'localhost',
-            port: 8080
-        },
-        proxy: {
-            '/api': {
-                target: 'your https address',
-                changeOrigin: true,
-                rewrite: (path: string) => path.replace(/^\/api/, '')
-            }
-        }
-    }
+  },
+  plugins: [vue(), template()],
+  server: {
+    port: 8080,
+    hmr: {
+      host: 'localhost',
+      port: 8080,
+    },
+    proxy: {
+      '/api': {
+        target: 'your https address',
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
